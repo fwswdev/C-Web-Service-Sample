36363d9b067ddcb93d13a812688d8bbdf8a2da15
aa <aa@aa.com>
2016-07-09 00:04:35 +0800

    first commit

---
+++ WebServicePractice/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WebServicePractice
+{
+    partial class frmApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnShow.Location = new System.Drawing.Point(16, 70);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(224, 23);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show Weather";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCity.Location = new System.Drawing.Point(68, 18);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(172, 20);
+            this.txtCity.TabIndex = 1;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCountry.Location = new System.Drawing.Point(68, 44);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(172, 20);
+            this.txtCountry.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(13, 109);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(32, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "result";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Country";
+            // 
+            // frmApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(260, 323);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.btnShow);
+            this.Name = "frmApp";
+            this.Text = "Weather Web Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WebServicePractice/Form1.cs
@@ -0,0 +1,41 @@
+﻿/* ================
+Web Service SOAP
+================= */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebServicePractice
+{
+    public partial class frmApp : Form
+    {
+        public frmApp()
+        {
+            InitializeComponent();
+            lblResult.Text = "";
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var city = txtCity.Text.Trim();
+            var country = txtCountry.Text.Trim();
+
+            if (city != String.Empty && country != String.Empty)
+            {
+                WeatherWebService.GlobalWeatherSoapClient client = new WeatherWebService.GlobalWeatherSoapClient();
+                var res = client.GetWeather(txtCity.Text, txtCountry.Text);
+                lblResult.Text = res;
+            }
+            else
+            {
+                MessageBox.Show("Please Enter The City and Country");
+            }
+        }
+    }
+}
